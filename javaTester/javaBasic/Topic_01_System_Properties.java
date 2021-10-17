03dc57d3dd906c0843c5f88fcceea513b650ea3d
Khan Nguyen <khanntm@gmail.com>
2021-10-17 07:02:07 +0700

    Create new repository

---
+++ javaTester/javaBasic/Topic_01_System_Properties.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_01_System_Properties {
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+		System.out.println(projectPath + "\\\\browserDrivers\\\\geckodriver.exe");
+
+	}
+
+}
