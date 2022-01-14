0b46a6fec7bf1ec7cbbc92d78fbeacc43bf9600f
Automation Testing <khanntm@gmail.ccom>
2022-01-14 11:00:54 +0700

    Wait Part I

--- selenium/webdriver/Topic_15_Handle_Popup.java
+++ selenium/webdriver/Topic_15_Handle_Popup.java
@@ -14,7 +14,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-
 public class Topic_15_Handle_Popup {
 	WebDriver driver;
 	WebDriverWait explicitWait;
