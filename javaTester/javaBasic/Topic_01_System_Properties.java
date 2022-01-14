0cfe78f8bbeb63cfd48bf9b6b12fee2f84927620
Automation Testing <khanntm@gmail.ccom>
2022-01-14 14:06:42 +0700

    Update if existed Java Basic 01

--- javaTester/javaBasic/Topic_01_System_Properties.java
+++ javaTester/javaBasic/Topic_01_System_Properties.java
@@ -5,6 +5,10 @@ public class Topic_01_System_Properties {
 	public static void main(String[] args) {
 		String projectPath = System.getProperty("user.dir");
 		System.out.println(projectPath);
+		
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
+		
 		System.out.println(projectPath + "\\\\browserDrivers\\\\geckodriver.exe");
 
 	}
