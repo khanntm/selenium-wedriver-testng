a824d5575884233eb2c58b0be76af25ae4261b4f
Automation Testing <khanntm@gmail.ccom>
2022-01-16 05:24:32 +0700

    Topic 17 Part IV

---
+++ selenium/webdriver/Topic_23_17_Wait_PartII.java
@@ -0,0 +1,83 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class Topic_23_17_Wait_PartII {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		
+	}
+
+	@Test
+	public void TC_01_Timeout_Less_Than_Element_Display() {
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.cssSelector("div#start>button")).click();
+			       
+		//After clicking spent 5s de display Helloword
+		Assert.assertTrue(driver.findElement(By.cssSelector("div#finish>h4")).isDisplayed());
+		
+	}
+
+	@Test
+	public void TC_02_Timeout_Equal_Element_Display() {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.cssSelector("div#start>button")).click();
+			       
+		//After clicking spent 5s de display Helloword
+		Assert.assertTrue(driver.findElement(By.cssSelector("div#finish>h4")).isDisplayed());
+	}
+
+	@Test
+	public void TC_03_Timeout_More_Than_Display() {
+		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.cssSelector("div#start>button")).click();
+			       
+		//After clicking spent 5s de display Helloword
+		Assert.assertTrue(driver.findElement(By.cssSelector("div#finish>h4")).isDisplayed());
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	public void sleepInSeconds(long second)
+	{
+		try {
+			Thread.sleep(second * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void sleepInMiliseconds(long milisecond)
+	{
+		try {
+			Thread.sleep(milisecond);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
