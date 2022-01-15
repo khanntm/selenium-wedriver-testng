e35b1f162fc067c9011db843f16751b35e9cd655
Automation Testing <khanntm@gmail.ccom>
2022-01-16 05:30:03 +0700

    Topic 17 Part V

---
+++ selenium/webdriver/Topic_23_17_Wait_PartV_Explicit.java
@@ -0,0 +1,112 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class Topic_23_17_Wait_PartV_Explicit {
+	WebDriver driver;
+	WebDriverWait explicitWait;
+	String projectPath = System.getProperty("user.dir");
+	
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		//Tim thấy là pass ko cần chờ hết timeout 
+		//10s là apply để tìm lại element, sau 10s nếu explicitWait ko tìm thấy sẽ failed
+		explicitWait = new WebDriverWait(driver,6);
+		
+	}
+
+	//@Test
+	public void TC_01_Invisile() {
+		
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.cssSelector("div#start>button")).click();
+		
+		//Wait cho loading icon iến mất
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div#loading")));
+		
+		//After clicking spent 5s de display Helloword
+		Assert.assertTrue(driver.findElement(By.cssSelector("div#finish>h4")).isDisplayed());
+		System.out.println(driver.findElement(By.cssSelector("div#finish>h4")).getText());
+		
+	}
+
+	//@Test
+	public void TC_02_Visile() {
+		
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.cssSelector("div#start>button")).click();
+		
+		//Chờ cho Helloword txt được hiển thị
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#finish>h4")));
+		
+		//After clicking spent 5s de display Helloword
+		Assert.assertTrue(driver.findElement(By.cssSelector("div#finish>h4")).isDisplayed());
+		System.out.println(driver.findElement(By.cssSelector("div#finish>h4")).getText());
+		
+	}
+
+	@Test
+	public void TC_03_Date_Picker() {
+		driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+		
+		//Wait until calendar display
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='ctl00_ContentPlaceholder1_Panel1']")));
+		
+		//In ra ngày đã chọn (before AJAx call) => hien tai chua chon nen in ra "No Selected Dates to display" 
+		//Wait cho den khi find dc element
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='15']")));
+		driver.findElement(By.xpath("//a[text()='15']")).click();
+		
+		//Wait the loading bar invisible div[id$='RadCalendar1']>div.raDiv 
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[contains(@id,'RadCalendar1')]//div[@class='raDiv']")));
+		//Check visible lable
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']")));
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']")).getText(), "Saturday, January 15, 2022");
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='15']/parent::td[@class='rcSelected']")).isDisplayed());
+		
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	public void sleepInSeconds(long second)
+	{
+		try {
+			Thread.sleep(second * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void sleepInMiliseconds(long milisecond)
+	{
+		try {
+			Thread.sleep(milisecond);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
